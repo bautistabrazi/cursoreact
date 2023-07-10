995f1ef05ebf294b7243f918f2a5cdbe3fae497b
bautista brazi <brazibautista@gmail.com>
2023-07-10 20:46:41 -0300

    finalizacion primer entrega

--- src/componente/Cartwidget/index.jsx
+++ src/componente/Cartwidget/index.jsx
@@ -1,10 +1,11 @@
 import '../CartWidget/style.css'
 
 const CartWidget= () => {
+    const contador = 0;
     return(
         <div className='carrito-container'>
-                    <img src="../imagenes/carrito2.png" alt="Carrito" className="img-carrito"/>
-                    <p className='cantidad'>1</p>
+            <img src="../imagenes/carrito2.png" alt="Carrito" className="img-carrito"/>
+            <span className='contad'>{contador}</span>
         </div>
     )
 }
--- src/componente/menuNav/index.jsx
+++ src/componente/menuNav/index.jsx
@@ -2,15 +2,29 @@ import '../menuNav/style.css'
 
 const Navbar = () => {
     return (
-        <nav className="nav">
-            <ul>
-                <li className="item1">Samsung</li>
-                <li className="item2">Apple</li>
-                <li className="item3">Motorola</li>
-                <img src="../imagenes/cel.png" alt="Phone" className="img-cart"></img>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Categorias
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Samsung</a></li>
+                    <li><a class="dropdown-item" href="#">Motorola</a></li>
+                    <li><a class="dropdown-item" href="#">Iphone</a></li>
+                </ul>
+                <li><img className="img-cart" src="../imagenes/cel.png" alt="" /></li>
             </ul>
-        </nav>
-
+            </div>
+        </div>
+    </nav>
     )
 }
 
