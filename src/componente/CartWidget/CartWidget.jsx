eae8071de9ee748a5566df23923689192e6a37b0
bautista brazi <brazibautista@gmail.com>
2023-08-18 22:15:25 -0300

    img-cart

--- src/componente/CartWidget/CartWidget.jsx
+++ src/componente/CartWidget/CartWidget.jsx
@@ -5,7 +5,7 @@ const CartWidget= () => {
     const {cartCantidad} = useContext (CartContext)
     return(
         <div className='carrito-container'>
-            <img src="https://i.postimg.cc/XZ0Q0K69/carrito.png" alt="Carrito" className="img-carrito"/>
+            <img src="https://i.postimg.cc/t4ky2rNX/carrito2.png" alt="Carrito" className="img-carrito"/>
         {cartCantidad() > 0 && <span className='contad'>{cartCantidad()}</span>}
         </div>
     )
