257b4f54def074dbd44c703e5850f8f1139f0520
bautista brazi <brazibautista@gmail.com>
2023-08-18 22:14:51 -0300

    imagenes redes

--- src/componente/Footer/Footer.jsx
+++ src/componente/Footer/Footer.jsx
@@ -2,10 +2,10 @@ const Footer = () => {
     return (
         <div className="footer">
             <ul className="list-footer">
-                <li className="item-img"><img src="../imagenes/fb2.png" alt="facebook" /></li>
-                <li className="item-img"><img src="../imagenes/ig.png" alt="instagram" /></li>
-                <li className="item-img"><img src="../imagenes/tw.png" alt="twitter" /></li>
-                <li className="item-img"><img src="../imagenes/yt.png" alt="youtube" /></li>
+                <li className="item-img"><img src="https://i.postimg.cc/Bvw3GXzk/fb2.png" alt="facebook" /></li>
+                <li className="item-img"><img src="https://i.postimg.cc/3rGTPrFf/ig.png" alt="instagram" /></li>
+                <li className="item-img"><img src="https://i.postimg.cc/8PGq9R5s/tw.png" alt="twitter" /></li>
+                <li className="item-img"><img src="https://i.postimg.cc/3JZPTk41/yt.png" alt="youtube" /></li>
             </ul>
             <p>&copy;2023</p>
         </div>
